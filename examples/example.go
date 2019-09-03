6cc956768cecb606b44435dec2267d7c5aa65428
Hao yun <hy352144278@gmail.com>
2019-09-03 13:08:13 +0800

    update: update example

--- examples/example.go
+++ examples/example.go
@@ -15,9 +15,15 @@ func main() {
 	defer glog.Flush()
 	wheel := timewheel.NewTimeWheel(3600)
 	go func() {
-		key := time.Now().Unix() + 2
-		glog.V(4).Infof("input task %d\n", key)
-		wheel.AddTask("123", key)
+		var key int64
+		now := time.Now().Unix()
+		for i := 20; i > 0; i = i - 5 {
+			for j := 0; j < 10; j++ {
+				key = now + int64(i)
+				wheel.AddTask(fmt.Sprintf("i: %d,j: %d", i, j), key)
+				glog.V(4).Infof("input task %d\n", key)
+			}
+		}
 	}()
 	wheel.Run(func(tasks []string) {
 		fmt.Println(tasks)
